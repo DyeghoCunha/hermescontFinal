e5a521e9600e2f52588e9dc7f272af885cb70b54
dyeghocunha <dyeghocunha@gmail.com>
2024-07-12 12:40:19 -0300

    fix

--- src/components/atoms/PageContainer/PageContainer.tsx
+++ src/components/atoms/PageContainer/PageContainer.tsx
@@ -5,7 +5,7 @@ interface IPageContainer {
 }
 export default function PageContainer({ children }: IPageContainer) {
   return (
-    <Center w="100vw" h={{base:"100vh", md:"110vh", xxl:"100vh"}}  bgGradient="linear(130deg, rgba(241,226,194,1) 0%, white 40%, rgba(242,242,243,1) 100%)" 
+    <Center w="100vw" h={{base:"100vh", md:"130vh", xxl:"100vh"}}  bgGradient="linear(130deg, rgba(241,226,194,1) 0%, white 40%, rgba(242,242,243,1) 100%)" 
     //mt="80px"
     >
       <VStack align="center"   justify="start" h="100%" w="100%" 
